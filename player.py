0e430dc587f1434f1b2512dff657a27451e6385f
Morgan <morgannetherway1234@gmail.com>
2017-10-17 13:01:13 +0100

    Start of player file

---
+++ player.py
@@ -0,0 +1,13 @@
+import * from items
+
+#players health - can be used globally to be modified by items/scrolls
+player_health = 100
+
+#players attack - can be used globally to be modified by items/scrolls
+player_attack = 10
+
+#players defence - can be used globally to be modified by items/scrolls
+player_defense = 10
+
+#player's inventory
+inventory = []
