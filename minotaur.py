8bd8ecef70f4dfd46e801d4aa189d868751619a2
Morgan <morgannetherway1234@gmail.com>
2017-10-17 13:02:53 +0100

    Start of minotaur file

---
+++ minotaur.py
@@ -0,0 +1,10 @@
+#import the rooms file
+
+#the current health for the minotaur
+minotaur_health = 100
+
+#the base damage the player will take
+minotaur_attack = 25
+
+#Minotaur starts at ..........
+#minotaur_current_location = 
