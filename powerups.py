4bf83c9562e2270bbdd7ab8a7fb1ab373c94d679
Morgan Netherway <morgannetherway1234@gmail.com>
2017-10-24 11:54:23 +0100

    Changed Minotaur key name

--- powerups.py
+++ powerups.py
@@ -1,3 +1,6 @@
-powerup_attack_boost = {}
-powerup_health_boost = {}
-powerup_minotaur_half_speed = {}
+powerup_attack_boost = {"name": "Attack Boost Potion", "description": "", "attack_boost": 1.5}
+
+powerup_health_boost = {"name": "Health Boost Potion", "description": "", "health_boost": 1.5}
+
+powerup_minotaur_half_speed = {"name": "Minotaur Speed Potion", "description": "",
+                               "minotaur_speed_boost": 0.5}
