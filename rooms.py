aecd6263e6de5a59f2d0ee0b5f8fd3658311975c
BuildTools <ieuangriffiths1230@gmail.com>
2017-10-25 00:06:27 +0100

    Bug fix

--- rooms.py
+++ rooms.py
@@ -1,3 +1,5 @@
+from items import *
+
 rooms = {
     "room_1": {
         "name": "Room 1",
@@ -9,7 +11,7 @@ rooms = {
         "name": "Room 1",
         "description": "blah blah blah",
         "items": [item_sword],
-        "action": special_well
+        "action": "special_well"
     },
     "room_3": {
         "name": "Room 1",
@@ -54,32 +56,32 @@ rooms = {
         "action": None
     },
     "room_10": {
-        "name": "Room 1",
-        "description": "blah blah blah",
+        "name": "MEDUSA’S CHAMBERS",
+        "description": "Medusa the deadly gorgon, with serpents as locks of hair, and a stare with the ability to turn people to stone was beheaded by the demigod son of Zeus, called Perseus. Athena, goddess of wisdom and war, then placed the head of Medusa on her shield, as the ultimate defence from her enemies. However it was stolen by King Minos, through Poseidon who felt pity for the king after ruining his life as well as being the natural rival of Athena. Minos placed the head in the Labyrinth in the hope that one day the Minotaur or travellers would stumble across it and turn to stone. Beware traveller, do not stare in her eyes, and watch out as the room is filled with statues of your journeying predecessors. """,
         "items": None,
         "action": None
     },
     "room_11": {
-        "name": "Room 1",
-        "description": "blah blah blah",
+        "name": "THE ROOM OF THE TITANS",
+        "description": """The predecessors of the generation of the Olympian deities, were the Titans. Kronos, father of Zeus, Hades and Poseidon, was overthrown by his children just as he had overthrown his father, lord of the sky, Uranus. However the Titans seek revenge from Olympian gods, and King Minos’ vengeful nature agreed him to house the spirit of Kronos, in this chamber of the Labyrinth, where not even the gods could imagine he was hiding. Beware traveller, Kronos is the lord of time, and even setting foot in this chamber without paying homage to the Titan would be foolish, as you could be trapped in a time loop within the deadly maze for eternity, not dying and simply being devoured by the Minotaur over and over till the end of time itself""",
         "items": None,
         "action": None
     },
     "room_12": {
-        "name": "Room 1",
-        "description": "blah blah blah",
+        "name": "FOUNTAIN OF DELPHI",
+        "description": """This chamber is an interesting one, one shall not pass without receiving a riddle from the prophecy giver the Oracle of Delphi herself. Beware traveller, she can hear your thoughts and play on your inner most secrets, physically you must be up to the task of beating the Minotaur, but mentally it is this chamber which you must overcome, stay clear of the green light and navigate carefully around the marble fountain, voices will be heard.""",
         "items": None,
         "action": None
     },
     "room_13": {
-        "name": "Room 1",
-        "description": "blah blah blah",
+        "name": "HOLE TO THE UNDERWORLD",
+        "description": """Poseidon played a cruel joke on King Minos by making his wife fall in love with a Cretan Bull, however his brother Hades, Lord of the Underworld granted King Minos a favour by creating a direct fall for travellers who enter the Labyrinth straight to the underworld. Beware travellers, it is a literal pit that leads you to hell and if you ended up in the Labyrinth you will likely be devoured by the three headed dog of Hades, Cerberus or walk the Asphodel fields of punishment for eternity.""",
         "items": None,
         "action": None
     },
     "room_14": {
-        "name": "Room 1",
-        "description": "blah blah blah",
+        "name": "TREASURE CHAMBER",
+        "description": """Due to the appearance and ferocity of the beast, everyone forgets the fact that the Minotaur itself is half human. Humans crave wealth and power, every being that the half bull half man has devoured, all their personal possessions and gold have been stored as a trophy cabinet in the Labyrinth. King Minos himself has not forgotten to appease his half son by ladening the room with the finest antiquities fit for the entrance room of Dionysus himself. The room has a light supply and the reflection is blinding, beware traveller do not touch the monsters riches, or be prepared to face the consequences.""",
         "items": None,
         "action": None
     }
