3172b392249cae84ebd58286b573b045badc9929
jrs5082 <salmonj10@hotmail.co.uk>
2017-10-25 05:52:02 +0100

    added diary descriptions

--- items.py
+++ items.py
@@ -81,7 +81,13 @@ item_diary_1 = {
     
     "name": "",
 
-    "description": "This student handbook explains everything. Seriously.",
+    "description":
+    """I, Theseus, son of Aegeus, come from the great metropolis of Athens to slay the beast
+known as the Minotaur, who resides at the heart of the Labyrinth of King Minos. Any trace of the outside
+world disappeared with the heavy bang of the limestone entrance heaving shut. The air is stale and suffocating,
+and no sound travels through it. The smell of rotting flesh crawls along the walls and clings to my body.
+The last set of sacrifices came here seven years ago, and by now they should be nothing but bone. Never mind.
+I came here to end the cycle. I, Theseus the Glorious, who knows no fear. I have truly set out to do the impossible.""",
 
     "mass": 1
  
@@ -90,10 +96,16 @@ item_diary_1 = {
 item_diary_2 = {
     "id": "diary_2",
 
-    "name": "a shield from a Spartan",
+    "name": "",
 
     "description":
-    "",
+    """It doesn't matter the direction you turn, whether left or right, forwards or backwards, you always end up at the same place.
+A glimmering blue well. It feels like I'm being toyed with. The silence is oppressing, slowly rising to a climax of nothing.
+I worry that I will be drained of strength before I even reach the Minotaur.
+I trace the cracks between the stones, and imagine how formidable the beast must be.
+It's almost as if I can see it stomping down these halls, it's cruel strength barely contained by the Labyrinth.
+Who built this place? Clearly no mortal man. If the Minotaur, who kills men for mere sustenance, cannot escape, then how can I hope to?
+But leaving was not what I volunteered for. I gave myself the goal of slaying the beast, and I will carry it out.""",
 
     "mass": 2
 }
@@ -103,7 +115,14 @@ item_diary_3 = {
     "name": "a shield from a Spartan",
 
     "description":
-    "",
+    """I listened. In between the silence, I found whispers. They told me the path forward.
+They tell of a hole leading straight to the Underworld, guarded by Cerebrus.
+They speak of a room full of previous trespassers, where the mythical Medusa's head stands guard.
+They even talk of a place in which time itself falls apart and recombines in strange ways.
+If I could pass all of these trials, I would find myself at the treasure chamber, and be rewarded with wealth beyond imagination.
+I didn't come for wealth. Every direction I was told to go, I went another. I remember, back at Athens, philosophers would posit endlessly.
+If a tree fell in the woods, and no one was around to hear it, did it fall at all? I write these diaries now to define myself.
+To make sure that my time here happened. """,
 
     "mass": 2
 }
@@ -113,7 +132,15 @@ item_diary_4 = {
     "name": "a shield from a Spartan",
 
     "description":
-    "",
+    """I've been here a long time. I'm not sure how long, as daylight doesn't visit this maze.
+Certainly long enough to feel emptiness of my stomach, or the dry, desperate thirst, or the vestiges of sleep.
+Yet, I haven't felt any of these. The passageways grow narrower, and the rooms look unfamiliar.
+The whispers are no longer whispers and have turned to thumps. I struggle to hear my own thoughts over the tumultuous din.
+Suddenly, I'm back at the well, but this time it's a vast sea. The sun beams down from above, but it gives off no warmth.
+The winds are still, but carry the smell of rot and viscera. I turn around, and on the floor lies a patch of straw-like fur, stained with drool.
+I remember why I'm here now. I'm comforted by the return of the Labyrinth walls, forming a cocoon of rock and stone around me.
+Less comforting is the sound of heavy breathing coming from further down the hall. The sound of air leaving its nostrils echoes towards me.
+It won't be long now.""",
 
     "mass": 2
 }
@@ -123,7 +150,10 @@ item_diary_5 = {
     "name": "a shield from a Spartan",
 
     "description":
-    "",
+    """I lost. The beast not only outmatched me in strength, but skill. It seemed to know every move I would make.
+I barely escaped, dispensing my gear and keeping only my sword for company. I should have bled out a long time ago.
+I lie here, bloodied and broken, apparently without the strength to even die.
+I suppose the architects of this prison still have a need for me.""",
     "mass": 2
 }
 item_diary_6 = {
@@ -136,3 +166,4 @@ item_diary_6 = {
 
     "mass": 2
 }
+print(item_diary_5["description"])
