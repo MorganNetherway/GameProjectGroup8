24751732e90f4c5cf8add4a1f0cf68801c6916bb
Morgan Netherway <morgannetherway1234@gmail.com>
2017-10-26 03:13:15 +0100

    commented

--- player.py
+++ player.py
@@ -1,31 +1,36 @@
-#import room file
+#imports the items so they be referenced from the player's inventory
 from items import *
 
+#creates a dictionary which contains various player stats, all used in the minotaur encounter
 player_stats = {"health": 100, "attack": 10, "defense": 10}
 
-#player's health
-#player_health = 100
 
-#player's attack
-#player_attack = 10
-
-#player's defence
-#player_defense = 10
-
-#player's inventory
+#player's inventory, empty at the start but gets appended to when items are picked up,
+#and items are popped out when dropped.
 inventory = []
 
-#player starts at ....
+#player starting location - location 7,0 on the game grid
 player_position = [7,0]
 
+#defines a function which is called whenever the player picks up an item.
+#it checks to see whether the item has a value modifier e.g. attack_value,
+#and if it does, the value in the player_stats dictionary is updated.
+
 def update_stats_take(item):
+
+    #makes variables global so they can be updated in the fucntion without being returned
     global minotaur_movement_speed
     global player_attack
     global player_defense
     global player_health
-    
+
+    #checks to see whether the corresponding key is present on the taken item
     if "attack_value" in item:
+        
+        #multiplies the relevant stat by the item modifier
         player_stats["attack"] *= item["attack_value"]
+        
+        #returns the updated dictionary to the main loop
         return player_stats
     
     if "defense_value" in item:
@@ -44,14 +49,25 @@ def update_stats_take(item):
         player_stats["health"] *= powerup_health_boost["health_boost"]
         return player_stats
 
+#defines a function which is called whenever the player drops an item.
+#it checks to see whether the item has a value modifier e.g. attack_value,
+#and if it does, the value in the player_stats dictionary is updated.
+    
 def update_stats_drop(item):
+    
+    #makes variables global so they can be updated in the fucntion without being returned
     global minotaur_movement_speed
     global player_attack
     global player_defense
     global player_health
-
+    
+    #checks to see whether the corresponding key is present on the taken item
     if "attack_value" in item:
+        
+        #divides the relevant stat by the item modifier
         player_stats["attack"] /= item["attack_value"]
+        
+        #returns the updated dictionary to the main loop
         return player_stats
     
     if "defense_value" in item:
